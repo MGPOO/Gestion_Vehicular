5f3726b312594e9be131124289da4a05b493cdaf
MGPOO <magutierrez9@espe.edu.ec>
2025-01-14 11:12:05 -0500

    Ralenti

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import VehiculoMas from './page/vehiculoMas';
 import Estacionamiento from './page/estacionamiento'
+import Ralenti from './page/ralenti'
 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Estacionamiento />} />
+        <Route path="/" element={< Ralenti />} />
       </Routes>
     </BrowserRouter>
   );
