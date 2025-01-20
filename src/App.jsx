d506b1614f4f9f60168fc85d39baaf04fbab0fb4
MGPOO <magutierrez9@espe.edu.ec>
2025-01-20 07:26:59 -0500

    A

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={< Ralenti />} />
+        <Route path="/" element={< Estacionamiento />} />
       </Routes>
     </BrowserRouter>
   );
